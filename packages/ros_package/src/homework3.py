6470d723fa43b9ee783c733f00a6d3191897a5b5
nayeyoni <Naye_Yoni@student.uml.edu>
2021-02-14 19:23:23 -0500

    hw3

--- packages/ros_package/src/homework3.py
+++ packages/ros_package/src/homework3.py
@@ -6,16 +6,15 @@ from mystery_package.msg import UnitsLabelled
 class homework3:
     def __init__(self):
         rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback)
-        pub = rospy.Publisher("homework3_output", UnitsLabelled, queue_size=10)
+        self.pub = rospy.Publisher("homework3_output", UnitsLabelled, queue_size=10)
         self.total = 0;
         self.pub_msg = UnitsLabelled()
         self.pub_msg.units = "feet"
         
     def callback(self, msg):
-        self.total = msg.data * 3.28084 
+        self.total = msg.value * 3.28084 
         self.pub_msg.value = self.total
-        self.pub_units.publish(self.pub_msg)
-        self.pub.publish(self.pub)
+        self.pub.publish(self.pub_msg)
         
 if __name__ == '__main__':
     rospy.init_node('homework3')
