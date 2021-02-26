8cc6072150dcbb1048732460e497333a582eaf8b
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-02-26 18:19:34 -0500

    hw4

--- packages/ros_package/src/homework4.py
+++ packages/ros_package/src/homework4.py
@@ -14,18 +14,26 @@ class homework4:
         if rospy.has_param("units"):
             self.units = rospy.get_param("units")
             if self.units == "smoots":
+                self.pub_msg.units = "smoots"
                 self.total = msg.value * 1.7018
                 self.pub_msg.value = self.total
                 self.pub.publish(self.pub_msg)
             elif self.units == "feet":
+                self.pub_msg.units = "feet"
                 self.total = msg.value * 3.28084 
                 self.pub_msg.value = self.total
                 self.pub.publish(self.pub_msg)
             else:
+                self.pub_msg.units = "meters"
                 self.total = msg.value  
                 self.pub_msg.value = self.total
                 self.pub.publish(self.pub_msg)
- 
+        else:
+            self.units = "default"
 if __name__ == '__main__':
     rospy.init_node('homework4')
-    homework4()
+    homework4()\
+    
+    rospy.spin()
+    
+    
