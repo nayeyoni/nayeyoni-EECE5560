0abf4d36dbe21f4d4b03abf124674db68ace44fd
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-02-26 15:14:47 -0500

    new_homework

--- packages/ros_package/src/homework3.py
+++ packages/ros_package/src/homework3.py
@@ -3,35 +3,21 @@
 import rospy
 from mystery_package.msg import UnitsLabelled
 
-class homework4:
+class homework3:
     def __init__(self):
         rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback)
-        self.pub = rospy.Publisher("homework4_output", UnitsLabelled, queue_size=10)
+        self.pub = rospy.Publisher("homework3_output", UnitsLabelled, queue_size=10)
         self.total = 0;
         self.pub_msg = UnitsLabelled()
+        self.pub_msg.units = "feet"
         
-        if rospy.has_param("units"):
-            self.units = rospy.get_param("units")
-                if self.units = "smoots":
-                    def callback(self, msg):
-                        self.total = msg.value * 1.7018
-                        self.pub_msg.value = self.total
-                        self.pub.publish(self.pub_msg)
-                elif self.units = "feet":
-                    def callback(self, msg):
-                        self.total = msg.value * 3.28084 
-                        self.pub_msg.value = self.total
-                        self.pub.publish(self.pub_msg)
-                else:
-                    def callback(self, msg):
-                        self.total = msg.value  
-                        self.pub_msg.value = self.total
-                        self.pub.publish(self.pub_msg)
-        else:
-            self.units = "default"
-            
+    def callback(self, msg):
+        self.total = msg.value * 3.28084 
+        self.pub_msg.value = self.total
+        self.pub.publish(self.pub_msg)
+        
 if __name__ == '__main__':
-    rospy.init_node('homework4')
+    rospy.init_node('homework3')
     homework3()
 
     rospy.spin()
---
+++ packages/ros_package/src/homework4.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import rospy
+from mystery_package.msg import UnitsLabelled
+
+class homework4:
+    def __init__(self):
+        rospy.Subscriber("/mystery/output2", UnitsLabelled, self.callback)
+        self.pub = rospy.Publisher("homework3_output", UnitsLabelled, queue_size=10)
+        self.total = 0;
+        self.pub_msg = UnitsLabelled()
+        
+    def callback(self, msg):
+        if rospy.has_param("units"):
+            self.units = rospy.get_param("units")
+            if self.units == "smoots":
+                self.total = msg.value * 1.7018
+                self.pub_msg.value = self.total
+                self.pub.publish(self.pub_msg)
+            elif self.units == "feet":
+                self.total = msg.value * 3.28084 
+                self.pub_msg.value = self.total
+                self.pub.publish(self.pub_msg)
+            else:
+                self.total = msg.value  
+                self.pub_msg.value = self.total
+                self.pub.publish(self.pub_msg)
+ 
+if __name__ == '__main__':
+    rospy.init_node('homework4')
+    homework4()
