33e2ee92e214acdaccfb67ddb245c8e346882e6c
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-03-10 23:53:11 -0500

    1

--- packages/ros_package/src/transform.py
+++ packages/ros_package/src/transform.py
@@ -10,10 +10,38 @@ class transform:
         rospy.Subscriber("point_sensor_coordinate", Vector2D, self.callback_function)
         self.pub_robot =rospy.Publisher("robot_coordinate_frame", Vector2d)
         self.pub_world =rospy.Publisher("world_coordinate_frame", Vector2d)
-        self.angle1=
+        angle1=radians(180)
+        angle2=(135)
+        self.a=cos(angle1)
+        self.b=-sin(angle1)
+        self.d=sin(angle1)
+        self.e=sin(angle1)
+        self.c=-1
+        self.f=0
+        self.g=0
+        self.h=0
+        self.i=1
         
+        self.j=cos(angle1)
+        self.k=-sin(angle1)
+        self.m=sin(angle1)
+        self.n=sin(angle1)
+        self.l=-10
+        self.o=5
+        self.r=1
+        self.p=0
+        self.q=0
+        
+        
+     
     def callback_funtion(self, msg):
-        msg.transform1=num
+        self.v = [[msg.x],[msg.y],[1]]
+        self.msg.transform1=numpy.matrix([a,b,c],[d,e,f],[g,h,i])
+        self.new_v1= self.msg.transform1*self.v
+        self.msg.transform2=numpy.matrix([j,k,l],[m,n,o],[p,q,r])
+        self.new_v2=self.msg.transform2*self.new_v1
+        self.pub_robot.publish(self.new_v1)
+        self.pub.world.publish(self.new_v2)
         
 if __name__ == '__main__':
     rospy.init_node('transform')
