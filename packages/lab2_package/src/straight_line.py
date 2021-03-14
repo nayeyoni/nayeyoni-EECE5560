73e7fa6c8de489d10d99742a4b36dab8d6767a7b
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-03-13 21:39:53 -0500

    a

--- packages/lab2_package/src/straight_line.py
+++ packages/lab2_package/src/straight_line.py
@@ -8,21 +8,21 @@ class line:
         self.pub = rospy.Publisher("/nayebot/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
         self.start = 0
         self.frame_id = ' '
-        self.header.seq = 0
-        self.header.stamp.secs = 0
-        self.header.stamp.nsecs = 0
+        self.Header.seq = 0
+        self.Header.stamp.secs = 0
+        self.Header.stamp.nsecs = 0
         while self.start < 6:
             self.omega = 0
             self.v=0.4099999964237213
             self.pub.publish(self.v)
             self.pub.publish(self.omega)
-            self.header.seq=self.header.seq+1
-            self.header.stamp.secs=self.header.stamp.secs+1
-            self.header.stamp.secs=self.header.stamp.nsecs+1
-            self.pub.publish(self.header.seq)
-            self.pub.publish(self.header.stamp.secs)
-            self.pub.publish(self.header.stamp.nsecs)
-            self.pub.publish(self.header.frame_id)
+            self.Header.seq=self.Header.seq+1
+            self.Header.stamp.secs=self.Header.stamp.secs+1
+            self.Header.stamp.secs=self.Header.stamp.nsecs+1
+            self.pub.publish(self.Header.seq)
+            self.pub.publish(self.Header.stamp.secs)
+            self.pub.publish(self.Header.stamp.nsecs)
+            self.pub.publish(self.Header.frame_id)
             self.start = self.start+1
 
 if __name__ == '__main__':
