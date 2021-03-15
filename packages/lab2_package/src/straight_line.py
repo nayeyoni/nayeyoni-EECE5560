7739ef616ec1f7eeaadcf547f8136940a2491384
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-03-15 13:57:45 -0400

    2

--- packages/lab2_package/src/straight_line.py
+++ packages/lab2_package/src/straight_line.py
@@ -7,15 +7,14 @@ from duckietown_msgs.msg import Twist2DStamped
 class line:
     def __init__(self):
         self.pub = rospy.Publisher("/nayebot/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+        self.pub_msg = Twist2DStamped()
         self.start = 0
-        self.header = std_msgs.msg.Header()
+        self.pub_msg.header = std_msgs.msg.Header()
         while self.start < 6:
-            self.header.stamp = rospy.Time.now()
-            self.v=0.4099999964237213
-            self.omega = 0
-            self.pub.publish(self.header)
-            self.pub.publish(self.v)
-            self.pub.publish(self.omega)
+            self.pub_msg.header.stamp = rospy.Time.now()
+            self.pub_msg.v=0.4099999964237213
+            self.pub_msg.omega = 0
+            self.pub.publish(self.pub_msg)
             self.start = self.start+1
 
 if __name__ == '__main__':
