53bbc4d1d75effde0ea460a7ff387506db258b04
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-03-15 23:30:28 -0400

    d

--- packages/lab2_package/src/straight_line.py
+++ packages/lab2_package/src/straight_line.py
@@ -2,6 +2,7 @@
 
 import rospy
 import std_msgs.msg
+from std_srvs.srv import SetBool
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
 class line:
@@ -11,23 +12,26 @@ class line:
         self.pub_msg = Twist2DStamped()
         self.start = 0
         self.pub_msg.header = std_msgs.msg.Header()
-   
-    def callback(self, mode):
-        if mode.state == 'LANE_FOLLOWING':
-            while self.start < 3:
+
+        if self.flag == True:
+            while self.start < 5:   
                 self.pub_msg.header.stamp = rospy.Time.now()
                 self.pub_msg.v=0.4099999964237213
                 self.pub_msg.omega = 0
                 self.pub.publish(self.pub_msg)
-                self.start = self.start+1
+            self.start = self.start+1
+
+    def callback(self, mode):
         if mode.state == 'LANE_FOLLOWING':
-            self.pub_msg.header.stamp = rospy.Time.now()
-            self.pub_msg.v=0
-            self.pub_msg.omega = 0
-            self.pub.publish(self.pub_msg)
+           self.flag = True
+        else:
+           self.flag = False  
 
 if __name__ == '__main__':
     rospy.init_node('straight_line' , anonymous=True)
     line()
-
-    rospy.spin()
+    if self.flag == False:
+        rospy.spin()
+    if self.flag == True:
+        rospy.signal_shutdown('Path is done')
+    
