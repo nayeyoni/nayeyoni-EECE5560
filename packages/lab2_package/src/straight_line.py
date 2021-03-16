e134bea55200480c789692f9ac92ae0b95897a51
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-03-16 18:05:01 -0400

    j

--- packages/lab2_package/src/straight_line.py
+++ packages/lab2_package/src/straight_line.py
@@ -6,16 +6,15 @@ from std_srvs.srv import SetBool
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
 
-
-
 class line:
     def __init__(self):
+        self.flag = SetBool() 
         rospy.Subscriber("/nayebot/fsm_node/mode", FSMState, self.callback)
         self.pub = rospy.Publisher("/nayebot/lane_controller_node/car_cmd", Twist2DStamped, queue_size=10)
         pub_msg = Twist2DStamped()
         pub_msg.header = std_msgs.msg.Header()
         pub_msg.header.stamp = rospy.Time.now()
-
+	
     def move_straight(self):
         pub_msg.v=0.4099999964237213
         pub_msg.omega = 0
@@ -27,20 +26,18 @@ class line:
         self.pub.publish(pub_msg)
 
     def callback(self, mode):
-        if mode.state == 'LANE_FOLLOWING':
-           flag = True
+        if mode.state == 'LANE_FOLLOWING': 
+           self.flag = True
         else:
-           flag = False  
+           self.flag = False  
 
 if __name__ == '__main__':
     rospy.init_node('straight_line' , anonymous=True)
-    flag = False
-    l=line()
-    start = 0
-    if flag == True:
-        while start < 4:
+    l = line()
+    time  = rospy.Time.now().secs
+    if self.flag == True:
+        while (rospy.Time.now().secs - time) < 4:
             l.move_straight()
-            start = start + 1
         l.stop()
     else:
         rospy.spin()
