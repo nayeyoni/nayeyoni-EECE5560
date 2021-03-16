b88ba88bfbbde42ee38b4cbfe1056554dac2378f
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-03-16 18:37:59 -0400

    j

--- packages/lab2_package/src/straight_line.py
+++ packages/lab2_package/src/straight_line.py
@@ -2,44 +2,27 @@
 
 import rospy
 import std_msgs.msg
-from std_srvs.srv import SetBool
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
-
-global flag
-
 class line:
     def __init__(self):
-        self.flag = SetBool() 
         rospy.Subscriber("/nayebot/fsm_node/mode", FSMState, self.callback)
         self.pub = rospy.Publisher("/nayebot/lane_controller_node/car_cmd", Twist2DStamped, queue_size=10)
-        pub_msg = Twist2DStamped()
-        pub_msg.header = std_msgs.msg.Header()
-        pub_msg.header.stamp = rospy.Time.now()
-	
-    def move_straight(self):
-        pub_msg.v=0.4099999964237213
-        pub_msg.omega = 0
-        self.pub.publish(pub_msg)
-        
-    def stop(self):
-        pub_msg.v=0
-        pub_msg.omega = 0
-        self.pub.publish(pub_msg)
+        self.pub_msg = Twist2DStamped()
+        self.pub_msg.header = std_msgs.msg.Header()
+        self.pub_msg.header.stamp = rospy.Time.now()
 
     def callback(self, mode):
-        if mode.state == 'LANE_FOLLOWING': 
-           flag = True
-        else:
-           flag = False  
+        time = rospy.Time.now().secs
+        if mode.state == 'LANE_FOLLOWING':
+            while (rospy.Time.now().secs - time) < 4:
+                self.pub_msg.v=0.4099999964237213
+                self.pub_msg.omega = 0
+                self.pub.publish(self.pub_msg)
+            rospy.signal_shutdown('Path is done')
 
 if __name__ == '__main__':
     rospy.init_node('straight_line' , anonymous=True)
-    l = line()
-    time  = rospy.Time.now().secs
-    if flag == True:
-        while (rospy.Time.now().secs - time) < 4:
-            l.move_straight()
-        l.stop()
-    else:
-        rospy.spin()
+    line()
+
+    rospy.spin()
