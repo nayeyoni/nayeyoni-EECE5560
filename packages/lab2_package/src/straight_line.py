426ae0133ed54e096cdcb4e82632523092dc0d7e
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-03-16 00:41:30 -0400

    j

--- packages/lab2_package/src/straight_line.py
+++ packages/lab2_package/src/straight_line.py
@@ -7,26 +7,24 @@ from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
 
 
-
+flag = False
 class line:
-    flag = False
-    def __init__(self):
+    
+    def __init__(self,flag):
         rospy.Subscriber("/nayebot/fsm_node/mode", FSMState, self.callback)
         self.pub = rospy.Publisher("/nayebot/lane_controller_node/car_cmd", Twist2DStamped, queue_size=10)
         self.pub_msg = Twist2DStamped()
         self.start = 0
         self.pub_msg.header = std_msgs.msg.Header()
         
-        if flag == True:
-            while self.start < 5:   
-                self.pub_msg.header.stamp = rospy.Time.now()
-                self.pub_msg.v=0.4099999964237213
-                self.pub_msg.omega = 0
-                self.pub.publish(self.pub_msg)
-            self.start = self.start+1
-
     def callback(self, mode):
-        if mode.state == 'LANE_FOLLOWING' and flag == False:
+        if mode.state == 'LANE_FOLLOWING':
+           while self.start < 5:   
+               self.pub_msg.header.stamp = rospy.Time.now()
+               self.pub_msg.v=0.4099999964237213
+               self.pub_msg.omega = 0
+               self.pub.publish(self.pub_msg)
+           self.start = self.start+1
            flag = True
         else:
            flag = False  
