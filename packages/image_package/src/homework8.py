7ec5808a05a64ea8ab7ef74202227909880d300e
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-04-09 10:59:22 -0400

    hw8

--- packages/image_package/src/homework8.py
+++ packages/image_package/src/homework8.py
@@ -20,7 +20,6 @@ class homework8:
         self.msg1_hsv = cv2.cvtColor(self.msg1,cv2.COLOR_BGR2HSV)
         self.canny_edge_img = cv2.Canny(self.msg1_hsv,100, 255)
         self.output_canny = self.bridge.cv2_to_imgmsg(self.canny_edge_img, "mono8")
-        self.pub.publish(self.output_canny)
         
         
     def callback2(self, msg2):
@@ -30,10 +29,8 @@ class homework8:
     def callback3(self, msg3):
         self.msg3 = self.bridge.imgmsg_to_cv2(msg3, "mono8")
         self.mask = cv2.bitwise_or(self.msg2, self.msg3)
-        self.canny = self.bridge.imgmsg_to_cv2(self.output_canny, "mono8")
-        self.output = cv2.bitwise_and(self.mask,self.canny)
-        self.output_mask = self.bridge.cv2_to_imgmsg(self.output, "mono8")
-
+        self.output_mask = self.bridge.cv2_to_imgmsg(self.mask, "mono8")
+        self.pub.publish(self.output_mask)
 
 if __name__=="__main__":
     
