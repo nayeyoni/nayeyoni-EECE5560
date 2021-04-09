dbffe552a7f46c7b82e52078454c74aba1512789
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-04-09 10:13:28 -0400

    hw8

--- packages/image_package/src/homework8.py
+++ packages/image_package/src/homework8.py
@@ -24,15 +24,12 @@ class homework8:
         
     def callback2(self, msg2):
         self.msg2 = self.bridge.imgmsg_to_cv2(msg2, "mono8")
-        self.msg2_hsv = cv2.cvtColor(self.msg2,cv2.COLOR_BGR2HSV)
         
         
     def callback3(self, msg3):
         self.msg3 = self.bridge.imgmsg_to_cv2(msg3, "mono8")
-        self.msg3_hsv = cv2.cvtColor(self.msg3,cv2.COLOR_BGR2HSV)
-        self.mask = cv2.bitwise_or(self.msg2_hsv, self.msg3_hsv)
-        self.output_mask = self.bridge.cv2_to_imgmsg(self.mask, "mono88")
-        self.pub.publish(self.output_mask)
+        self.mask = cv2.bitwise_or(self.msg2, self.msg3)
+        self.pub.publish(self.mask)
 
 if __name__=="__main__":
     
