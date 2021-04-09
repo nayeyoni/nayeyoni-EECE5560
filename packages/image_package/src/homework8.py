f139824daf596235373b973f62f0a320b71cdecd
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-04-08 22:21:41 -0400

    hw8

--- packages/image_package/src/homework8.py
+++ packages/image_package/src/homework8.py
@@ -9,8 +9,6 @@ from cv_bridge import CvBridge
 class homework8:
     def __init__(self):
         rospy.Subscriber("image_cropped", Image, self.callback1)
-        rospy.Subscriber("image_white", Image, self.callback2)
-        rospy.Subscriber("image_yellow", Image, self.callback3)
         self.pub = rospy.Publisher("image_output", Image, queue_size=10)
         self.bridge = CvBridge()
 
@@ -20,11 +18,7 @@ class homework8:
         self.canny_edge_img = cv2.Canny(self.msg1,100, 255)
         self.pub.publish(self.canny_edge_img)
         
-    def callback2(self, msg2):
-        self.msg2 = self.bridge.imgmsg_to_cv2(msg2, "bgr8")
-        
-    def callback3(self, msg3):
-        self.msg3 = self.bridge.imgmsg_to_cv2(msg3, "bgr8")
+
 
 if __name__=="__main__":
     
