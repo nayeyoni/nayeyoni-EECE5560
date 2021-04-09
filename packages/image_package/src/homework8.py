44826a93cc593b3ba9fc25d31131bfc3872a5d47
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-04-08 22:19:06 -0400

    hw8

--- packages/image_package/src/homework8.py
+++ packages/image_package/src/homework8.py
@@ -17,7 +17,7 @@ class homework8:
         
     def callback1(self, msg1):
         self.msg1 = self.bridge.imgmsg_to_cv2(msg1, "bgr8")
-        self.canny_edge_img = cv2.Canny(msg1,100, 255)
+        self.canny_edge_img = cv2.Canny(self.msg1,100, 255)
         self.pub.publish(self.canny_edge_img)
         
     def callback2(self, msg2):
