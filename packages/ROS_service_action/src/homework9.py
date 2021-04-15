3808698b5b0690fceff73dc942dee72ebac72592
Naye Yoni <Naye_Yoni@student.uml.edu>
2021-04-15 13:26:41 -0400

    hw9

--- packages/ROS_service_action/src/homework9.py
+++ packages/ROS_service_action/src/homework9.py
@@ -5,21 +5,28 @@ from example_service.srv import Fibonacci, FibonacciResponse
 import actionlib
 import example_action_server.msg
 
+def service(num):
+    rospy.wait_for_service('calc_fibonacci')
+    calc_fibonacci = rospy.ServiceProxy('calc_fibonacci', Fibonacci)
+    rospy.loginfo(rospy.get_caller_id() + "Order %s: Service starts", num)
+    resp1 = calc_fibonacci(num)
+    rospy.loginfo(rospy.get_caller_id() + "Order %s: Service ends and sequence is %s", num, resp1)
 
-if __name__ == "__main__":
-    try: 
-        rospy.init_node('homework9')
-    
-        client = actionlib.SimpleActionClient('fibonacci', example_action_server.msg.FibonacciAction)
-        client.wait_for_server()
-      
-        goal = example_action_server.msg.FibonacciGoal(order=15)
+def client(order):
+    client = actionlib.SimpleActionClient('fibonacci', example_action_server.msg.FibonacciAction)
+    client.wait_for_server()
+    rospy.loginfo(rospy.get_caller_id() + "Sending goal with order %s", num)
+    goal = example_action_server.msg.FibonacciGoal(order=num)
+    client.send_goal(goal)
+    client.wait_for_result()
+    rospy.loginfo(rospy.get_caller_id() + "Result with order %s: %s", num, client.get_result() )
+    return client.get_result()  
 
-        client.send_goal(goal)
-        client.wait_for_result()
+if __name__=="__main__":
+    rospy.init_node("homework9", anonymous=True)
+    service(3)
+    client(3)
+    service(15)
+    client(15)
+    rospy.spin()
 
-        result = client.get_result()
-        print("Raa")
-        print("Result:", ', '.join([str(n) for n in result.sequence])) 
-    except rospy.ROSInterruptException:
-        pass
