8fdbbf2fd9ea217322756a6b720c7471c012f902
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-04-26 11:51:02 -0400

    hw10

--- packages/controller/src/pid_controller.py
+++ packages/controller/src/pid_controller.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Float32
 class pid_controller:
     def __init__(self):
         rospy.set_param("controller_ready", "true")
-        self.p = pid_class(Kp = 0.05, Ki = 0.13, Kd = 7)  
+        self.p = pid_class(Kp = 0.05, Ki = 0.13, Kd = 7.5)  
         self.pub1 = rospy.Publisher("/control_input", Float32, queue_size=10)
         rospy.Subscriber("/error", Float32, self.callback)
         
