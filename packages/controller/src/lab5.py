d90ee447a468539152a255cfd66d23239f964ab1
nayeyoni <nayeyoni@nayeyoni-VirtualBox>
2021-04-27 23:11:34 -0400

    l5

---
+++ packages/controller/src/lab5.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import rospy
+from pid_class import pid_class
+from std_msgs.msg import Float32
+from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import FSMState
+from duckietown_msgs.msg import LanePose
+
+class lab5:
+    def __init__(self):
+        rospy.Subscriber("fsm_node/mode", FSMState, self.state)
+        rospy.Subscriber("/lane_filter_node/lane_pose", LanePose, self.callback)
+        self.pub = rospy.Publisher("/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=10)
+        self.d = pid_class(Kp = 0, Ki = 0, Kd = 0)  
+        self.phi = pid_class(Kp = 0, Ki = 0, Kd = 0) 
+        self.d_value = 0
+        self.phi_value = 0
+        car_control_msg = Twisted2DStamped()
+        self.lane_following_is_ON = False
+        
+    def state (self, mode):
+        if mode.state == 'LANE_FOLLOWING':
+            self.lane_following_is_ON = True
+            rospy.logwarn("NAYE'S NODE")
+        else 
+            self.lane_following_is_ON = False
+    
+    def callback(self, error):
+        self.phi_value = error.phi 
+        self.d_value = error.d
+        if self.lane_following_is_ON = True:
+            acc1 = self.p.update(self.d_value, 0.1)
+            acc2 = self.p.update(self.phi_value, 0.1)
+            car_control_msg.v = 0.1
+            car_control_msg.omega = acc1 + acc2
+            self.pub1.publish(car_control_msg)
+        
+if __name__ == '__main__':
+    rospy.init_node('lab5', anonymous=True)
+    lab5()
+    rospy.spin()
